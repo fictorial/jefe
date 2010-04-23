d47b27e7f6698b5a057b3f87fb16178e12f283b2
Brian Hammond <brian@fictorial.com>
2010-04-23 16:48:30 -0400

    Simple example is working.

--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,15 @@
 var 
   sys = require("sys"),
   jefe = new require("../lib/jefe"),
-  elJefe = new jefe.Jefe();
+  elJefe = new jefe.Jefe(),
+  scriptName = "set a sandboxed global"; 
 
-elJefe.addScript("set a sandboxed global", "FOOBAR=1");
+elJefe.addScript(scriptName, "FOOBAR=1");
+
+elJefe.runScript(scriptName, { FOOBAR: 2 }, function (error, response) {
+  if (error)  
+    sys.log("[EXAMPLE/SIMPLE] error = " + error);
+  else 
+    sys.log("[EXAMPLE/SIMPLE] response = " + JSON.stringify(response));
+});
 
